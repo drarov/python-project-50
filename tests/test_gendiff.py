8fbd88d6480f487f71e4b495d20acfc7000e98ea
drarov <drarov@mail.ru>
2023-04-18 15:02:02 +0500

    In this commit: tests are improved by adding a correct path to files

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,9 +1,10 @@
 from gendiff import generate_diff
+import os
 
 
 def test_generate_diff():
-    file_path1 = 'file1.json'
-    file_path2 = 'file2.json'
+    file_path1 = os.path.join(os.path.dirname(__file__), 'file1.json')
+    file_path2 = os.path.join(os.path.dirname(__file__), 'file2.json')
     diff = generate_diff(file_path1, file_path2)
     expected_output = '''{
   - follow: false
@@ -17,8 +18,8 @@ def test_generate_diff():
 
 
 def test_generate_diff2():
-    file_path1 = 'file1.json'
-    file_path2 = 'file1.json'
+    file_path1 = os.path.join(os.path.dirname(__file__), 'file1.json')
+    file_path2 = os.path.join(os.path.dirname(__file__), 'file1.json')
     diff = generate_diff(file_path1, file_path2)
     expected_output = '''{
     follow: false
@@ -30,8 +31,8 @@ def test_generate_diff2():
 
 
 def test_generate_diff3():
-    file_path1 = 'file1.json'
-    file_path2 = 'file3.json'
+    file_path1 = os.path.join(os.path.dirname(__file__), 'file1.json')
+    file_path2 = os.path.join(os.path.dirname(__file__), 'file3.json')
     diff = generate_diff(file_path1, file_path2)
     expected_output = '''{
   - follow: false
@@ -46,8 +47,8 @@ def test_generate_diff3():
 
 
 def test_generate_diff_emptyfile():
-    file_path1 = 'file1.json'
-    file_path2 = 'file4.json'
+    file_path1 = os.path.join(os.path.dirname(__file__), 'file1.json')
+    file_path2 = os.path.join(os.path.dirname(__file__), 'file4.json')
     diff = generate_diff(file_path1, file_path2)
     expected_output = '''{
   - follow: false
